b8db942f65b8e0a2f0a93ea4bf35905ac4571950
Jose Carlos <51250539+jcarlos5@users.noreply.github.com>
2019-10-20 18:57:31 -0500

    index

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function buscar(){
+    $("#body").removeClass("color");
+    $("#logo").addClass("logo-min");
+    $("#search-bar").addClass("form-control-sm");
+    $("#btn-buscar").addClass("btn-sm");
+    $("#cabecera").removeClass("mt-5");
+    $("#cabecera").addClass("mt-2");
+    $("#bar-cab").addClass("col-5");
+    $("#bar-cab").removeClass("col-10");
+    $("#cabecera").removeClass("justify-content-center");
+    $("#cont-main").removeClass("col-12");
+    $("#cont-main").addClass("col-10");
+    $("#no-importa").addClass("pl-4");
+    $("#resultados").removeClass("hidden");
+    $("#perfil").removeClass("flotando");
+    $("#perfil").addClass("col-2");
+}
